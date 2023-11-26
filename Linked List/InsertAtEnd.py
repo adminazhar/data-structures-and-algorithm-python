9a4c7ae73d16136ca4012515b508740292159500
adminazhar <admin@azhark.com>
2023-11-27 00:39:43 +0530

    Linked List - Insert At End

---
+++ Linked List/InsertAtEnd.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+# newll = LinkedList(10)
+newll = LinkedList()
+print(newll)
+newll.append(10)
+newll.append(20)
+newll.append(30)
+print(newll.length)
