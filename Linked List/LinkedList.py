97fd95e3d1470427d3f89570f4cd89b5bedc4a1f
adminazhar <admin@azhark.com>
2023-11-27 00:39:29 +0530

    Linked List

---
+++ Linked List/LinkedList.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 0
+
+newll = LinkedList(15)
+print(newll.head.value)
+print(newll.tail.value)
